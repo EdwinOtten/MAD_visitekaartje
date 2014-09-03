f172c2f2dc494e8e478c1c61b8434c81f96e3bb6
edwinanon <edwinnname@Academie-voor-Technologie-en-Managements-iMac-3.local>
2014-09-03 15:35:01 +0200

    layout finished.

---
+++ visitekaartje/visitekaartje/Naw.h
@@ -0,0 +1,13 @@
+//
+//  Naw.h
+//  visitekaartje
+//
+//  Created by Edwinn Name on 03/09/14.
+//  Copyright (c) 2014 Edwinn Name. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Naw : NSObject
+
+@end
---
+++ visitekaartje/visitekaartje/Naw.m
@@ -0,0 +1,13 @@
+//
+//  Naw.m
+//  visitekaartje
+//
+//  Created by Edwinn Name on 03/09/14.
+//  Copyright (c) 2014 Edwinn Name. All rights reserved.
+//
+
+#import "Naw.h"
+
+@implementation Naw
+
+@end
